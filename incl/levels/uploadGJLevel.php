029e6f231ca87bd276fa40fae09e3dc6a481b708
Wubba-jpeg <Pdxfxa@gmail.com>
2026-07-25 17:02:39 +0100

    Update uploadGJLevel.php

--- incl/levels/uploadGJLevel.php
+++ incl/levels/uploadGJLevel.php
@@ -74,8 +74,10 @@ $ts = !empty($_POST["ts"]) ? ExploitPatch::number($_POST["ts"]) : 0;
 if(isset($_POST["password"])) $password = $_POST["password"] != 0 ? ExploitPatch::remove($_POST["password"]) : 1;
 else $password = $gameVersion > 21 ? 1 : 0;
 $id = $gs->getIDFromPost();
+if ($usesaccounts === 1) {
 if (stripos($id, 's-') === 0) exit("-1");
 if (strpos($id, 'ffffffff-') !== 0 && strpos($id, '00000000-') !== 0) exit("-1");
+}
 $hostname = $gs->getIP();
 $userID = $gs->getUserID($id, $userName);
 $checkBan = $gs->getPersonBan($id, $userID, 2);
