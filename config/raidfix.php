f125c020dd7f4435bea204f5fc047ac6c416e754
Wubba-jpeg <Pdxfxa@gmail.com>
2026-07-25 17:02:12 +0100

    Update raidfix.php

--- config/raidfix.php
+++ config/raidfix.php
@@ -5,5 +5,6 @@ $commentperhr = 15; /* comments per hr*/
 $likesperhr = 10;/* likes each hr*/
 $accperhr = 10; /* accounts per hr*/
 $starsbuffer = 50; /* when submitting your stars, if your stars are above the max amount of stars one can possibly gain, you're blocked. however, a buffer is here for dailies / weeklies / events, etc. (if you're even using this for 1.9+). */
+$usesaccounts = 0; /* if you use accounts, set this to 1. */
 $discordWebhook = ""; /*webhook where logs for raids are sent. if you don't want these, keep this empty*/
 // I'll (soon) add more choices here. if you want anything to be added, make an issue in the repo
